57b6e4b9f8ea9bd0255e7723e0e6ee66616bdb07
Mohammad Zulqurnain <mohammad.zulqurnain@gmail.com>
2019-07-18 13:51:56 +0500

    Initial Commit

---
+++ PersonalityTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityTest
+//
+//  Created by Mohammad Zulqarnain on 18/07/2019.
+//  Copyright © 2019 Mohammad Zulqarnain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
